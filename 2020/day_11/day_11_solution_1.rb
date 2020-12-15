eb22e8b58b39ac46836adfdcc6e62c300b4a85b8
David Pastl <davidpastl@Davids-MacBook-Pro.local>
2020-12-14 19:36:03 -0600

    Got test working for day 11

---
+++ 2020/day_11/day_11_solution_1.rb
@@ -0,0 +1,81 @@
+OCCUPIED_SEAT = '#'
+EMPTY_SEAT = 'L'
+FLOOR = '.'
+
+def print_seating(seating)
+    for row in seating
+        for item in row
+            print "#{item}"
+        end
+        print "\n"
+    end
+end
+
+def count_occupied(seating)
+    total = 0
+    for row in seating
+        total += row.count(OCCUPIED_SEAT)
+    end
+    return total
+end
+
+def count_adjacent(seating, check_row, check_column)
+    adjacent_count = 0
+    for row in check_row-1..check_row+1
+        if row >= 0 and row < seating.length
+            for column in check_column-1..check_column+1
+                if column >= 0 and column < seating[row].length
+                    unless row == check_row and column == check_column
+                        if seating[row][column] == OCCUPIED_SEAT
+                            adjacent_count += 1
+                        end
+                    end
+                end
+            end
+        end
+    end
+    return adjacent_count
+end
+
+def update_seating(seating)
+    new_seating = seating.clone.map(&:clone)
+    change_count = 0
+    for row in 0..seating.length-1
+        for column in 0..seating[0].length-1
+            unless seating[row][column] == FLOOR
+                nearby_count = count_adjacent(seating, row, column)
+                # print "Row: #{row}, Column: #{column}, Count: #{nearby_count}\n"
+                if nearby_count == 0 and seating[row][column] != OCCUPIED_SEAT
+                    new_seating[row][column] = OCCUPIED_SEAT
+                    change_count += 1
+                elsif nearby_count >= 4 and seating[row][column] != EMPTY_SEAT
+                    new_seating[row][column] = EMPTY_SEAT
+                    change_count += 1
+                end
+            end
+        end
+    end
+    return new_seating, change_count
+end
+
+seating = File.readlines("test_file.txt").map{|str| str.chomp}.map{|str| str.chars}
+
+# print "Length: #{seating.length}, Width: #{seating[0].length}\n"
+# print "adjacent_count: #{count_adjacent(seating, 0, 0)}\n"
+
+# print_seating(seating)
+# print "Next\n"
+# new_seating, change_count = update_seating(seating)
+# print_seating(new_seating)
+# print "Change Count: #{change_count}\n"
+
+changes = 1
+iterations = 0
+while changes != 0 and iterations < 10
+    # print "Next\n"
+    # print_seating(seating)
+    seating, changes = update_seating(seating)
+    iterations += 1
+end
+print "Changes: #{changes}, Iterations: #{iterations}, Total Occupied: #{count_occupied(seating)}\n"
+print_seating(seating)
--- 2020/day_13/day_13_solution_2.rb
+++ 2020/day_13/day_13_solution_2.rb
@@ -1,4 +1,4 @@
-lines = File.readlines('test_file.txt')
+lines = File.readlines('input_13.txt')
 raw_buses = lines[1].split(",")
 buses = []
 for i in 0..raw_buses.length-1
@@ -7,37 +7,6 @@ for i in 0..raw_buses.length-1
     end
 end
 print "#{buses}\n"
-
-def next_arrival(bus, current_time)
-    closest_index = ((current_time).to_f/bus[0]).ceil
-    return closest_index * bus[0]
-end
-
-def list_correct(buses, depart_list)
-    list_correct = true
-    for i in 0..buses.length-2
-        unless depart_list[0] + buses[i+1][1] == depart_list[i+1]
-            list_correct = false
-        end
-    end
-    return list_correct
-end
-
-# t1 = Time.now
-# time_index = 0
-# solution_found = false
-# while not solution_found
-#     time_index += 1
-#     t = buses[0][0]*time_index
-#     depart_list = [t]
-#     for i in 1..buses.length-1
-#         depart_list.push(next_arrival(buses[i], t))
-#     end
-#     solution_found = list_correct(buses, depart_list)
-# end
-# print "Time Index: #{time_index}, Time: #{time_index*buses[0][0]}\n"
-# t2 = Time.now
-# print "Time: #{t2-t1}\n"
 
 t1 = Time.now
 time_index = 0
@@ -54,14 +23,3 @@ end
 print "Time Index: #{time_index}, Time: #{time_index*buses[0][0]}\n"
 t2 = Time.now
 print "Time: #{t2-t1}\n"
-
-# t1 = Time.now
-# time = 1068781
-# depart_list = []
-# for i in 1..buses.length-1
-#     depart_list.push(((time+buses[i][1])%buses[i][0]).zero?)
-# end
-# print "Depart List: #{depart_list}\n"
-# print "List Correct: #{depart_list.include? false}\n"
-# t2 = Time.now
-# print "Time: #{t2-t1}\n"
